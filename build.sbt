4769f755227f61f3062e881b3ccdb9a23ce63849
w313062006 <w313062006@gmail.com>
2016-07-07 10:34:57 +0800

    first commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "365recom_I"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
---
+++ src/main/scala/helloworld.scala
@@ -0,0 +1,9 @@
+/**
+  * Created by wushuang on 16-7-7.
+  */
+
+object helloworld {
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+  }
+}
